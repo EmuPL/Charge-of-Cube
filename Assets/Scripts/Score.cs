e28b97e3d726abd1f6fd18b5043d7c8ec5598e39
EmuPL <walczak.piotr765@gmail.com>
2022-12-28 15:22:17 +0100

    Fixed Score.cs
    Fixing Score.cs

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -3,9 +3,10 @@ using UnityEngine.UI;
 using TMPro;
 public class Score : MonoBehaviour
 {
-public Transform Player;
-public TextMeshProUGUI ScoreText;
-    void Update() 
+    public Transform Player;
+    public TextMeshProUGUI ScoreText;
+
+    private void Update()
     {
         ScoreText.text = Player.position.z.ToString("0");
     }
