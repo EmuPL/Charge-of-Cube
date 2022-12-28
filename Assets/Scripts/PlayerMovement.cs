88b93d04c09199c270ebbbf93abfed2760c95232
EmuPL <walczak.piotr765@gmail.com>
2022-12-28 16:34:04 +0100

    Fixed PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,24 +2,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
-
-    public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
-
-    void FixedUpdate()
+    public Rigidbody PlayerRigidbody = null;
+    private const float UnderPlatform = -1f;
+    public float ForwardForce = 2000f;
+    public float SidewaysForce = 500f;
+    private void FixedUpdate()
     {
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        PlayerRigidbody.AddForce(0, 0, ForwardForce * Time.deltaTime);
         
         if(Input.GetKey("d"))
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            PlayerRigidbody.AddForce(SidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         if(Input.GetKey("a"))
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            PlayerRigidbody.AddForce(-SidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
-        if(rb.position.y < -1f)
+        if(PlayerRigidbody.position.y < UnderPlatform)
         {
             FindObjectOfType<GameManager>().EndGame();
         }
