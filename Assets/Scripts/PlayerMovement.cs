cc7e7b2d39a9397e9d7636dc775bd4eeba7e2922
EmuPL <walczak.piotr765@gmail.com>
2022-12-31 12:25:56 +0100

    Fix PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,6 @@ public class PlayerMovement : MonoBehaviour
     public float ForwardForce = 2000f;
     public float SidewaysForce = 500f;
 
-    private const float UNDER_PLATFORM = 0f;
-    
     private void FixedUpdate()
     {
         PlayerRigidbody.AddForce(0, 0, ForwardForce * Time.deltaTime);
@@ -20,11 +18,6 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey("a"))
         {
             PlayerRigidbody.AddForce(-SidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-        if (PlayerRigidbody.position.y < UNDER_PLATFORM)
-        {
-            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
